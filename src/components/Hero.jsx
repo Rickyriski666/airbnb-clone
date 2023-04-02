7aa3ad6ef93fed42f64d862af810245ae9b30d8a
Ricky Riskiawan <rickyrizkiawan666@gmail.com>
2023-04-02 08:48:50 +0700

    make airbnb clone

---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+export default function Hero() {
+  return (
+    <div className="container my-8 mx-auto">
+      <div class="grid grid-rows-3 grid-cols-5">
+        {/* <div class="grid grid-rows-3 grid-flow-col gap-4">
+          <div class="row-start-2 row-span-2 ...">01</div>
+          <div class="row-end-3 row-span-2 ...">02</div>
+          <div class="row-start-1 row-end-4 ...">03</div>
+        </div> */}
+
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+          {/* <img src="src\assets\images\hero1.png" alt="" /> */}
+        </div>
+        <div class="col-start-2">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <div className="w-full h-full bg-[url('E:\Learning\freecodecamp\airbnb-clone\src\assets\images\hero1.png')] bg-cover bg-center"></div>
+        </div>
+        <div class="">
+          <img src="src\assets\images\hero9.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="border-red-500 bg-white py-6 px-8 drop-shadow-md">
+      <img src="src\assets\Vector.png" alt="" />
+    </nav>
+  );
+}
